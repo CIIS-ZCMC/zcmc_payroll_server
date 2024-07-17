a18e88fbffae04456b8fdf96a0c22352b3f3d696
kimdee <dhoracekay@gmail.com>
2024-07-17 16:26:38 +0800

    migration table

---
+++ app/Models/DeductionLog.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DeductionLog extends Model
+{
+    use HasFactory;
+}
---
+++ app/Models/EmployeeComputedSalary.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class EmployeeComputedSalary extends Model
+{
+    use HasFactory;
+}
