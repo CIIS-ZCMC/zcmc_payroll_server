ec30ec1c2d1a74e25be328b012f64a8964dabb31
Kim Horace A. Dolar <kimhorace@zcmcad.local>
2024-07-18 16:04:46 +0800

    new updates

---
+++ app/Http/Middleware/StripTags.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class StripTags
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $cleanData = [];
+
+        foreach ($request->all() as $key => $value) {
+            if (empty($value)) {
+                $cleanData[$key] = $value;
+                continue;
+            }
+
+            if (is_int($value)) {
+                $cleanData[$key] = $value;
+                continue;
+            }
+            
+            if (is_array($value)) {
+                $cleanData[$key] = $value;
+                continue;
+            }
+
+            if (strtotime($value)) {
+                $datetime = Carbon::parse($value);
+                $cleanData[$key] = $datetime->format('Y-m-d'); // Adjust the format as needed
+                continue;
+            }
+            
+
+            $cleanData[$key] = strip_tags($value);
+        }
+
+        $request->merge($cleanData);
+        
+        return $next($request);
+    }
+}
